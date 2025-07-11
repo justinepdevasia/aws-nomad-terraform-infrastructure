70b8bde35938b9df6c78f9a92faf6c1d4af99ecc
jpd <40311683+justinepdevasia@users.noreply.github.com>
2025-07-11 18:00:07 -0400

    Update qa dynamodb.tf to replace itsy with app

--- tf-remote-state/qa/dynamodb/dynamodb.tf
+++ tf-remote-state/qa/dynamodb/dynamodb.tf
@@ -6,8 +6,8 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_dynamodb_table" "terraform-locks-itsy-qa-us-west-1" {
-  name         = "terraform-locks-itsy-qa-us-west-1"
+resource "aws_dynamodb_table" "terraform-locks-app-qa-us-west-1" {
+  name         = "terraform-locks-app-qa-us-west-1"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
